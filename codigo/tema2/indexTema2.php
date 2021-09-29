11d64c1a26515815418889e1a2606b0bd8bea737
uriel <uriel.palalo@educa.jcyl.es>
2021-09-29 16:02:32 +0000

    hoals

--- codigo/tema2/indexTema2.php
+++ codigo/tema2/indexTema2.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="webroot/css/style.css">
+    <link rel="stylesheet" href="../../webroot/css/style.css">
     <title>Mi pagina principal</title>
 </head>
 <body>
-    <header>Página de Uriel</header>
+    <header>
+        Página de Uriel
+        <p class="subtitulo">Index tema 2</p>
+
+    </header>
     <main>
-        <a href="codigo/tema2/indexTema2.html">Tema 2</a>
+        <a href=""></a>
+        <?php
+            phpinfo();
+        ?>
     </main>
     <footer>&copy urielpalacios</footer>
 </body>
