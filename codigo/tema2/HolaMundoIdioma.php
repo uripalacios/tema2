c7c7c4da256978a098a948691c33a3890b691ff7
uriel <uriel.palalo@educa.jcyl.es>
2021-10-04 17:40:41 +0000

    commit idiomas acabados

---
+++ codigo/tema2/HolaMundoIdioma.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PR.02</title>
+</head>
+<body>
+    <?php
+
+        $idiomaRecogido= $_GET['idioma'];
+        $idiomas_es = "Hola";
+        $idiomas_en = "Hello";
+        $idiomas_pt = "Olá";
+        $idiomas_al = "Hallo";
+        $idiomas_fr = "Salut";
+
+
+
+        //Variable de saludo que es la que se escribe
+        // variable de variable
+        $saludo="idiomas_".$idiomaRecogido ;
+        echo $$saludo;
+        
+    ?>
+</body>
+</html>
